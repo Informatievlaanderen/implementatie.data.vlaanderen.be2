a86efe606612681970cb2301a9570fa417104e1b
OSLO-support <OSLO-support@vlaanderen.be>
2026-05-06 11:22:04 +0200

    Raw publications of existing publications

--- scripts/turn2raw.sh
+++ scripts/turn2raw.sh
@@ -3,4 +3,4 @@
 FILE=$1
 COMMIT=$2
 
-jq --arg branchtag "$COMMIT"  -r ' .[] |=  . + {"type": "raw"} + {"directory" : .urlref} + {"repository":"https://github.com/Informatievlaanderen/OSLO-Generated" } + {"branchtag" : "\($branchtag)" } ' ${FILE}
+jq --arg branchtag "$COMMIT"  -r ' .[] |=  . + {"type": "raw"} + {"directory" : .urlref} + {"repository":"https://github.com/Informatievlaanderen/implementatie.data.vlaanderen.be-generated" } + {"branchtag" : "\($branchtag)" } ' ${FILE}
